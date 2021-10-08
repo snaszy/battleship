2722023fee89b2f134911cfe5fe89bc8f6730644
Snaszy <scottnasman@gmail.com>
2021-10-07 22:06:35 -0600

    working through the addCoordinatesToLocation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 console.log('hello world')
 
+let battleship = {
+    length: 4,
+    location: [
+        {x: 2, y: 'B'},
+        {x: 3, y: 'B'},
+        {x: 4, y: 'B'},
+        {x: 5, y: 'B'}
+    ],
+    vertical: false,
+    hit: [],
+    sunk: false
+};
+
 const ship = (object) => {
    
 }
@@ -11,7 +24,10 @@ const hit = (object, coordinates) => {
 const sunk = (object) => {
     if (object.length - object.hit.length === 0) {
         object.sunk = true;
+    } else {
+        object.sunk = false;
     }
+    return object.sunk;
 }
 
 const createShip = (length) => {
@@ -27,33 +43,45 @@ const createShip = (length) => {
 const gameBoard = (object) => {
    
 };
+const startingLocation = (object) => {
+    object.location.push({x: 2, y: "B"})
+}
 
 const modifyCoordinates = (object) => {
     if (object.vertical) {
-        object.x++
+        addLocationX(object);
     } else {
-        object.y = nextChar(object.y)
+        addLocationY(object);   
     }
 };
+
+const addLocationX = (object) => {
+    object.location[0].x++;
+}
+
+const addLocationY = (object) => {
+    object.location[0].y = nextChar(object.location[0].y);
+}
 
 const nextChar = (c) => {
-    String.fromCharCode(c.charCodeAt(0) + 1)
+    return String.fromCharCode(c.charCodeAt(0) + 1)
 };
 
-const addCoordinatesToLocation = (ship, startingLocation) => {
-    let currentLocation
-    for (let i = 0; i < ship.length; i++) {
-        ship.location.push(currentLocation);
-        currentLocation = modifyCoordinates(currentLocation);
+const addCoordinatesToLocation = (object, startingLocation) => {
+    let currentLocation = startingLocation;
+    for (let i = 0; i < object.length; i++) {
+        object.location.push(currentLocation);
+        modifyCoordinates(currentLocation);
     }
+    return object.location
 };
 
 module.exports = {
-    ship,
-    gameBoard,
     sunk,
     hit,
+    createShip,
+    startingLocation,
     modifyCoordinates,
     nextChar,
-    addCoordinatesToLocation,
+    addCoordinatesToLocation
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,7 @@
 const { expect } = require('@jest/globals');
-const sunk = require('./index');
-const gameBoard = require('./index');
+const { sunk, hit, createShip, modifyCoordinates, nextChar, addCoordinatesToLocation } = require('./index')
 
-let battleship = {
+const battleship = {
     length: 4,
     location: [
         {x: 2, y: 'B'},
@@ -15,6 +14,56 @@ let battleship = {
     sunk: false
 };
 
-it('sunk returns true', () => {
-    expect(sunk()).toBe(battleship.sunk);
+it('sunk returns false', () => {
+    expect(sunk(battleship)).toBe(false);
+})
+it ('hit to push coordinates', () => {
+    const coordinates = {x:2, y:'B'};
+    hit(battleship, coordinates)
+    expect(battleship.hit).toEqual([{x:2, y:'B'}])
+})
+it ('creates ship', () => {
+    expect(createShip(4)).toEqual(
+        {
+            length: 4,
+            vertical: false, 
+            location: [],
+            hit: [],
+            sunk: false,
+        }
+    )
+})
+it ('add to x cordinates', () => {
+    const verticalShip = {
+        vertical: true,
+        location: [{x: 2, y: 'B'}]
+    };
+    modifyCoordinates(verticalShip)
+    expect(verticalShip.location).toEqual([{x: 3, y: 'B'}]);
+});
+it ('returns the next character', () => {
+    expect(nextChar('B')).toEqual('C');
+})
+it ('add to y cordinates', () => {
+    const horizontalShip = {
+        vertical: false,
+        location: [{x: 2, y: 'B'}]
+    };
+    modifyCoordinates(horizontalShip)
+    expect(horizontalShip.location).toEqual([{x: 2, y: 'C'}]);
+});
+it ('pushes all coordinates to locations of object', () => {
+    const miniShip = {
+        length: 4,
+        vertical: false,
+        location: []
+    }
+    const start = {x: 2, y: 'B'};
+    addCoordinatesToLocation(miniShip, start);
+    expect(miniShip.location).toContain([
+        {x: 2, y: 'B'},
+        {x: 3, y: 'B'},
+        {x: 4, y: 'B'},
+        {x: 5, y: 'B'},
+    ])
 })
